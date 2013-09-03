c01123cbdf32f26e6b01ed96fae77f9cd47ae1b3
Karlis Lauva <skazhy@gmail.com>
2013-09-03 23:32:22 +0300

    Add dummy images
    Success kid and Mercury pose, can't
    go wrong with these, righ?

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,14 +7,27 @@ object Application extends Controller {
 
   def allTags : List[String] = List("wat", "win")
 
+  def allImages : Map[String, List[String]] = {
+      Map("win" -> List(
+        "http://i.imgur.com/iOjD0q1.png",
+        "http://i.imgur.com/KdxWrIZ.png"
+      ))
+  }
+
+  def imagesForTag(tag: String) : List[String] = {
+      allImages.getOrElse(tag, List())
+  }
+
   def index = Action {
     val tags = allTags
-    Ok(views.html.index(tags))
+    val images = allImages.map(_._2).flatten.toList
+    Ok(views.html.index(tags, images))
   }
 
   def tag(tag: String) = Action {
     val tags = allTags
-    Ok(views.html.tag(tag, tags))
+    val images = imagesForTag(tag)
+    Ok(views.html.tag(tag, tags, images))
   }
 
 }
